0a0bacc4cd90388cf637911e451b426694e95610
Adrian Popan <adrian.popan@gmail.com>
2019-06-11 21:55:47 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -31,9 +31,10 @@ end
 def hit?
   prompt_user
   get_user_input
-  if get_user_input == "s"
+  user_input = get_user_input
+  if user_input == "s"
     card_total 
-  elsif get_user_input == "h"
+  elsif user_input == "h"
     card_total += deal_card
   else
     invalid_command
