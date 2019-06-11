3cd63c57b09c3064d31f28608b102972758ce733
Adrian Popan <adrian.popan@gmail.com>
2019-06-11 22:16:06 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,52 +1,52 @@
-def welcome
-  puts "Welcome to the Blackjack Table"
-end
+# def welcome
+#   puts "Welcome to the Blackjack Table"
+# end
 
-def deal_card
-  rand(1..11)
-end
+# def deal_card
+#   rand(1..11)
+# end
 
-def display_card_total(card_total)
-  puts "Your cards add up to #{card_total}"
-end
+# def display_card_total(card_total)
+#   puts "Your cards add up to #{card_total}"
+# end
 
-def prompt_user
-  puts "Type 'h' to hit or 's' to stay"
-end
+# def prompt_user
+#   puts "Type 'h' to hit or 's' to stay"
+# end
 
-def get_user_input
-  gets.chomp.strip
-end
+# def get_user_input
+#   gets.chomp.strip
+# end
 
-def end_game(card_total)
-  puts "Sorry, you hit #{card_total}. Thanks for playing!"
-end
+# def end_game(card_total)
+#   puts "Sorry, you hit #{card_total}. Thanks for playing!"
+# end
 
-def initial_round
-  card_total = deal_card + deal_card
-  display_card_total(card_total)
-  return card_total
-end
+# def initial_round
+#   card_total = deal_card + deal_card
+#   display_card_total(card_total)
+#   return card_total
+# end
 
-def hit?
-  prompt_user
-end
+# def hit?
+#   prompt_user
+# end
 
-def invalid_command
-  puts "Please enter a valid command"
-end
+# def invalid_command
+#   puts "Please enter a valid command"
+# end
 
-#####################################################
-# get every test to pass before coding runner below #
-#####################################################
+# #####################################################
+# # get every test to pass before coding runner below #
+# #####################################################
 
-def runner
-  welcome
-  initial_round
-  until card_total > 21
-    card_total = hit?(card_total)
-    display_card_total(card_total)
-  end
-  end_game(card_total)
-end
+# def runner
+#   welcome
+#   initial_round
+#   until card_total > 21
+#     card_total = hit?(card_total)
+#     display_card_total(card_total)
+#   end
+#   end_game(card_total)
+# end
     
